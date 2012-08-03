f580b7767aaa31ecbd58bcac5def5577200c6782
RHGP <rhgpally@mail.com>
2012-08-03 15:33:42 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cac6384988f45b7f5092cc131d183eea12da43edc2472aa868d53bf29d530e7f5097134a3a287f9ab8376e420f20543672079237adaf783b1edce015a2f232b6'
